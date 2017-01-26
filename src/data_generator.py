4556cbf2b76f8721b167b8f70cce4b2e81ef4fe7
ascane <mandy00724@gmail.com>
2017-01-26 21:59:55 +0100

    Add loss function.

--- src/data_generator.py
+++ src/data_generator.py
@@ -7,20 +7,26 @@ class DataGenerator(object):
         self.n = n
         self.k = k
         self.theta = theta
+        self.v = None
         self.X = None
         
-    def getX(self):
+    def get_X(self):
         if self.X is None:
             self._generate()
         return self.X
     
+    def get_v(self):
+        if self.v is None:
+            self._generate()
+        return self.v
+    
     def _generate(self):
-        v = np.zeros(self.p)
+        self.v = np.zeros(self.p)
         for i in range(self.k):
-            v[i] = 1 / sqrt(self.k)
-        sigma = np.identity(self.p) + self.theta * v[:, np.newaxis] * v[np.newaxis, :]
+            self.v[i] = 1 / sqrt(self.k)
+        sigma = np.identity(self.p) + self.theta * self.v[:, np.newaxis] * self.v[np.newaxis, :]
         self.X = np.random.multivariate_normal(np.zeros(self.p), sigma, self.n)
 
 if __name__ == "__main__":
     dg = DataGenerator(2, 5, 2)
-    print dg.getX()
+    print dg.get_X()
--- src/main.py
+++ src/main.py
@@ -2,13 +2,25 @@ from math import floor, sqrt
 
 import data_generator, sdp_estimator
 
+def loss(u, v):
+    return sqrt(1 - (u.dot(v)) ** 2)
+
 if __name__ == "__main__":
     p = 50
-    n = 50
+    n = 334
     k = int(floor(sqrt(p)))
     dg = data_generator.DataGenerator(p, n, k)
-    X = dg.getX()
+    X = dg.get_X()
+    v = dg.get_v()
+    print "X = "
     print X
+    print "v = "
+    print v
 
     sdp = sdp_estimator.SdpEstimator(X, lbda=0.1, eps=0.1)
-    print sdp.get_sdp()
+    v_sdp = sdp.get_sdp()
+    print "v_sdp = "
+    print v_sdp
+    
+    print "loss = "
+    print loss(v, v_sdp)
