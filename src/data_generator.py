ce2b208e9deebfea62125b7cdb8af81c05ff70fb
ascane <mandy00724@gmail.com>
2017-01-29 22:57:36 +0100

    Add the option of adding v2 in the covariance matrix.

--- src/data_generator.py
+++ src/data_generator.py
@@ -2,11 +2,13 @@ import numpy as np
 from math import sqrt
 
 class DataGenerator(object):
-    def __init__(self, p, n, k, theta=1.):
+    def __init__(self, p, n, k, theta=1., second=False, theta2=0.1):
         self.p = p
         self.n = n
         self.k = k
         self.theta = theta
+        self.second = second
+        self.theta2 = theta2
         self.v = None
         self.X = None
         
@@ -29,6 +31,10 @@ class DataGenerator(object):
         for i in range(self.k):
             self.v[i] = 1 / sqrt(self.k)
         sigma = np.identity(self.p) + self.theta * self.v[:, np.newaxis] * self.v[np.newaxis, :]
+        if self.second:
+            for i in range(self.p):
+                self.v2[i] = 1 / sqrt(self.p)
+            sigma += self.theta2 * self.v2[:, np.newaxis] * self.v2[np.newaxis, :]
         self.X = np.random.multivariate_normal(np.zeros(self.p), sigma, self.n)
 
 if __name__ == "__main__":
