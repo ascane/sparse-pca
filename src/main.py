def5bec6fecad525fe323a51f4d07a8c75735900
ascane <mandy00724@gmail.com>
2017-01-30 23:19:13 +0100

    Remove the option of second.

--- src/main.py
+++ src/main.py
@@ -6,21 +6,21 @@ import data_generator, sdp_estimator
 def loss(u, v):
     return sqrt(1 - (u.dot(v)) ** 2)
 
-def mean_loss_lin(p, nu, n_iter=1, verbose=False, output=None, second=False):
+def mean_loss_lin(p, nu, n_iter=1, verbose=False, output=None):
     k = int(floor(sqrt(p)))
     n = int(floor(nu * k * np.log10(p)))
-    return _mean_loss(p, n, k, n_iter, verbose, output, second)
+    return _mean_loss(p, n, k, n_iter, verbose, output)
 
-def mean_loss_quad(p, nu, n_iter=1, verbose=False, output=None, second=False):
+def mean_loss_quad(p, nu, n_iter=1, verbose=False, output=None):
     k = int(floor(sqrt(p)))
     n = int(floor(nu * k * k * np.log10(p)))
-    return _mean_loss(p, n, k, n_iter, verbose, output, second)
+    return _mean_loss(p, n, k, n_iter, verbose, output)
 
-def _mean_loss(p, n, k, n_iter, verbose, output, second):
+def _mean_loss(p, n, k, n_iter, verbose, output):
     if not(output is None):
         output.write("p = %s, n = %s, k = %s \n" %(p, n, k))
     print "p = %s, n = %s, k = %s" %(p, n, k)
-    dg = data_generator.DataGenerator(p, n, k, second)
+    dg = data_generator.DataGenerator(p, n, k)
     loss_sum = 0
     for i in range(n_iter):
         if not(output is None):
@@ -49,6 +49,6 @@ if __name__ == "__main__":
     _p = 50
     with open("../output/p=" + str(_p) + ".txt", "w") as f: 
         for _nu in range(50, 1001, 50):
-            mean_l = mean_loss_lin(p=_p, nu=_nu, n_iter=100, verbose=False, output=f, second=True)
+            mean_l = mean_loss_lin(p=_p, nu=_nu, n_iter=100, verbose=False, output=f)
             f.write("mean loss = %f \n" %mean_l)
             print "mean loss = %f" %mean_l
